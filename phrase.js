e89921909ee39b76fa0ec598a066344abac02bda
posibiliste <60448059+posibiliste@users.noreply.github.com>
2022-03-23 12:48:03 +0000

    Add files via upload

--- phrase.js
+++ phrase.js
@@ -1,31 +1,65 @@
 function showPhrase(value) {
-    var rangeInput = document.getElementById("q1in");
+    var rangeInput = document.getElementById("range1");
     var value = rangeInput.value;
 
     if (value == 0) {
-        document.getElementById("phrase").innerHTML="zero";
+        document.getElementById("phrase1feat").innerHTML="0 Absent during observation";
     }
     if (value == 1) {
-        document.getElementById("phrase").innerHTML="one";
+        document.getElementById("phrase1feat").innerHTML="1 Very low amount of this feature during observation";
     }
 
     if (value == 2) {
-        document.getElementById("phrase").innerHTML="two";
+        document.getElementById("phrase1feat").innerHTML="2 Low amount of this feature during observation";
     }
 
     if (value == 3) {
-        document.getElementById("phrase").innerHTML="three";
+        document.getElementById("phrase1feat").innerHTML="3 Average amount of this feature during observation";
     }
 
     if (value == 4) {
-        document.getElementById("phrase").innerHTML="four";
+        document.getElementById("phrase1feat").innerHTML="4 More than average amount of this feature during observation";
     }
 
     if (value == 5) {
-        document.getElementById("phrase").innerHTML="five";
+        document.getElementById("phrase1feat").innerHTML="5 Much more than average amount of this feature during observation";
     }
     if (value == 6) {
-        document.getElementById("phrase").innerHTML="six";
+        document.getElementById("phrase1feat").innerHTML="6 Very high amount of this feature during observation";
     }
 
-}
+  }
+
+
+
+  function showPhrase1a(value) {
+      var rangeInput = document.getElementById("range1a");
+      var value = rangeInput.value;
+
+      if (value == 0) {
+          document.getElementById("phrase1part").innerHTML="-3 I am so focused on this feature that it completely interferes with my participation";
+      }
+      if (value == 1) {
+          document.getElementById("phrase1part").innerHTML="-2 This feature moderately interferes with my participation";
+      }
+
+      if (value == 2) {
+          document.getElementById("phrase1part").innerHTML="-1 This feature mildly interferes with my participation";
+      }
+
+      if (value == 3) {
+          document.getElementById("phrase1part").innerHTML="0 It does not seem to affect my participation";
+      }
+
+      if (value == 4) {
+          document.getElementById("phrase1part").innerHTML="1 This feature mildly supports my participation";
+      }
+
+      if (value == 5) {
+          document.getElementById("phrase1part").innerHTML="2 This feature moderately supports my participation";
+      }
+      if (value == 6) {
+          document.getElementById("phrase1part").innerHTML="3 This feature completely supports my participation";
+      }
+
+    }
---
+++ phrase2.js
@@ -0,0 +1,65 @@
+function showPhrase2(value) {
+    var rangeInput = document.getElementById("range2");
+    var value = rangeInput.value;
+
+    if (value == 0) {
+        document.getElementById("phrase2feat").innerHTML="0 Absent during observation";
+    }
+    if (value == 1) {
+        document.getElementById("phrase2feat").innerHTML="1 Very low amount of this feature during observation";
+    }
+
+    if (value == 2) {
+        document.getElementById("phrase2feat").innerHTML="2 Low amount of this feature during observation";
+    }
+
+    if (value == 3) {
+        document.getElementById("phrase2feat").innerHTML="3 Average amount of this feature during observation";
+    }
+
+    if (value == 4) {
+        document.getElementById("phrase2feat").innerHTML="4 More than average amount of this feature during observation";
+    }
+
+    if (value == 5) {
+        document.getElementById("phrase2feat").innerHTML="5 Much more than average amount of this feature during observation";
+    }
+    if (value == 6) {
+        document.getElementById("phrase2feat").innerHTML="6 Very high amount of this feature during observation";
+    }
+
+  }
+
+
+
+  function showPhrase2a(value) {
+      var rangeInput = document.getElementById("range2a");
+      var value = rangeInput.value;
+
+      if (value == 0) {
+          document.getElementById("phrase2part").innerHTML="-3 I am so focused on this feature that it completely interferes with my participation";
+      }
+      if (value == 1) {
+          document.getElementById("phrase2part").innerHTML="-2 This feature moderately interferes with my participation";
+      }
+
+      if (value == 2) {
+          document.getElementById("phrase2part").innerHTML="-1 This feature mildly interferes with my participation";
+      }
+
+      if (value == 3) {
+          document.getElementById("phrase2part").innerHTML="0 It does not seem to affect my participation";
+      }
+
+      if (value == 4) {
+          document.getElementById("phrase2part").innerHTML="1 This feature mildly supports my participation";
+      }
+
+      if (value == 5) {
+          document.getElementById("phrase2part").innerHTML="2 This feature moderately supports my participation";
+      }
+      if (value == 6) {
+          document.getElementById("phrase2part").innerHTML="3 This feature completely supports my participation";
+      }
+
+    }
